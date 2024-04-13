a62b5bafca8b82e0d8e8cb2ead923ef731919a66
glynis <contact@glyn.is>
2024-04-13 16:10:41 -0400

    Initial Commit

---
+++ TempConvert/TempConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConvertApp.swift
+//  TempConvert
+//
+//  Created by Glynis Jones on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TempConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
