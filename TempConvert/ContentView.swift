29cee6f62e3dc56fcf55b588ec8d922314d2af78
glynis <contact@glyn.is>
2024-04-14 02:15:38 -0400

    Simple temperature conversion app for practice

--- TempConvert/ContentView.swift
+++ TempConvert/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @FocusState private var tempIsFocussed: Bool
+    @State private var inputTemp = 0
+    @State private var inputUnit: inputUnits = .farenheitStart
+//    @State private var outputTemp = 0
+//    let tempUnits = ["Farenheit to Celsius", "Celsius to Fahrenheit"]
+    
+//had to look up this enum bit but it isn't copypasta code
+    enum inputUnits: String, CaseIterable {
+        case farenheitStart = "Farenheit to Celsius"
+        case celsiusStart = "Celsius to Farenheit"
+    }
+    
+    var outputTemp: Int {
+        var convertResult = 0
+        
+        switch inputUnit {
+        case .farenheitStart:
+            convertResult = (inputTemp - 32) / 2
+        case .celsiusStart:
+            convertResult = (inputTemp * 2) + 32
         }
-        .padding()
+        
+        return convertResult
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Choose Conversion Direction") {
+                    Picker("F / C", selection: $inputUnit) {
+                        ForEach(inputUnits.allCases, id: \.self) {
+                            Text($0.rawValue)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Enter Temperature") {
+                    TextField("Temp", value: $inputTemp, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($tempIsFocussed)
+                }
+                Section("Converted Temperature") {
+                    Text("\(outputTemp)")
+                }
+                
+            }
+            .navigationTitle("Temperature Converter")
+            .toolbar {
+                if tempIsFocussed {
+                    Button("Done") {
+                        tempIsFocussed = false
+                    }
+                    }
+                }
+        }
     }
 }
 
